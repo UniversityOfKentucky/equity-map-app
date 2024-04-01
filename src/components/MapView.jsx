062e06f7f095360dc71f95cb7a756c1c2d4f6514
Phillip <96359059+phillipashford@users.noreply.github.com>
2024-03-31 21:36:25 -0400

    Adjust leaflet default zoom

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -7,7 +7,7 @@ const MapView = () => {
 
   return (
     <div className="h-screen w-screen">
-      <MapContainer center={position} zoom={10} style={{ height: '100%', width: '100%' }}>
+      <MapContainer center={position} zoom={11} style={{ height: '100%', width: '100%' }}>
         <TileLayer
           url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attributions">CARTO</a>'
