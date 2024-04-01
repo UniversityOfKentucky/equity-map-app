f2d24e669184e2deabc11473e48915e0dff63103
Phillip <96359059+phillipashford@users.noreply.github.com>
2024-04-01 11:13:58 -0400

    Import and insert IndicatorsPanel component

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 import React from 'react';
 import './App.css'
 import MapView from './components/MapView';
+import IndicatorsPanel from './components/IndicatorsPanel';
 
 function App() {
   return (
     <div className="App h-screen w-screen">
       <MapView />
+      <IndicatorsPanel />
     </div>
   );
 }
