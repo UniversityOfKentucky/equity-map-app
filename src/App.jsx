d07c72905546d2dd4ebb94eb633a9356507f9919
Phillip <96359059+phillipashford@users.noreply.github.com>
2024-04-01 12:42:43 -0400

    Import and insert FeaturesPanel component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import './App.css'
 import MapView from './components/MapView';
 import IndicatorsPanel from './components/IndicatorsPanel';
+import FeaturesPanel from './components/FeaturesPanel';
 
 function App() {
   return (
     <div className="App h-screen w-screen">
       <MapView />
       <IndicatorsPanel />
+      <FeaturesPanel />
     </div>
   );
 }
