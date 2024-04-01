325aba0651c991bf2b149be12e2054123c6ffa03
Phillip <96359059+phillipashford@users.noreply.github.com>
2024-03-31 21:29:03 -0400

    Import and insert MapView component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import React from 'react';
 import './App.css'
+import MapView from './components/MapView';
 
 function App() {
   return (
     <div className="App h-screen w-screen">
+      <MapView />
     </div>
   );
 }
