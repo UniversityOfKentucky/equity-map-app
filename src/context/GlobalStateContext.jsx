42fcc7fad44c9a446b6cad883757d35e48755d08
Phillip <96359059+phillipashford@users.noreply.github.com>
2024-04-03 10:58:11 -0400

    Refactor global state management
    Removed unused state variables and hooks. Updated the component import path in the comments.

--- src/context/GlobalStateContext.jsx
+++ src/context/GlobalStateContext.jsx
@@ -3,32 +3,17 @@ import React, { createContext, useState, useContext } from 'react';
 // Creates a context object to store the shared state variables.
 export const GlobalStateContext = createContext();
 
-// Creates a hook which returns the context value.
-export const useGlobalState = () => useContext(GlobalStateContext);
-
-// Once imported into app.js, the GlobalStateProvider component will wrap app.js's child components, allowing them to access the shared state variables.
+// Once imported into main.jsx, the GlobalStateProvider component will wrap main.jsx's child components, allowing them to access the shared state variables.
 export const GlobalStateProvider = ({ children }) => { // This function manages the shared state variables and returns their values to the children components via props.
 
     // Shared state variables and their initial values
     // Stores the selected category
     const [selectedCategory, setSelectedCategory] = useState('Population');
-    // Stores the expanded category
-    const [expandedCategory, setExpandedCategory] = useState('Population');
-    // Stores the selected feature
-    const [selectedFeature, setSelectedFeature] = useState('');
-    // Stores the modal open/close state
-    const [modalIsOpen, setModalIsOpen] = useState(false);
 
     // Value to be passed to provider consumers
     const value = {
         selectedCategory,
         setSelectedCategory,
-        expandedCategory,
-        setExpandedCategory,
-        selectedFeature,
-        setSelectedFeature,
-        modalIsOpen,
-        setModalIsOpen,
     };
 
     return (
