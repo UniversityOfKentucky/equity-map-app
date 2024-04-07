cd26aebb9b744e8e601ec59d52de844ca170a6a8
Phillip <96359059+phillipashford@users.noreply.github.com>
2024-04-07 14:25:07 -0400

    Enhance map interaction and accuracy
    Added useRef for map interactions, adjusted initial map position for precision, and configured map properties for improved user control, such as zoom limitations and scroll behavior.

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -1,10 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { MapContainer, TileLayer, GeoJSON, Popup } from 'react-leaflet'; 
 import 'leaflet/dist/leaflet.css';
 import FeaturesPanel from './FeaturesPanel';
 
 const MapView = () => {
-  const position = [38.0406, -84.5037];
+
+  const mapRef = useRef(null);
+
+  const position = [38, -84.5037];
 
   // Array destructuring to set the state of 'geojsonData' and 'setGeojsonData' function
   const [geojsonData, setGeojsonData] = useState(null);
@@ -19,7 +22,7 @@ const MapView = () => {
 
   return (
     <div className="col-span-4 relative h-screen">
-      <MapContainer center={position} zoom={11} style={{ height: '100%', width: '100%' }}>
+      <MapContainer center={position} zoom={11} style={{ height: '100%', width: '100%' }} minZoom={11} maxZoom={19} scrollWheelZoom={false} maxBounds={[[38.25, -84.75], [37.75, -84.25]]} ref={mapRef}>
         <TileLayer
           url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attributions">CARTO</a>'
