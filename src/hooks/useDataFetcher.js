cd7df4992eafda85a319b68283565043c85f247e
Phillip <96359059+phillipashford@users.noreply.github.com>
2024-04-07 22:13:33 -0400

    Refactor useDataFetcher hook
    Enhanced useDataFetcher to support an optional custom updater function for flexible state updates. Replaced static API key with environment variable for security.

--- src/hooks/useDataFetcher.js
+++ src/hooks/useDataFetcher.js
@@ -1,25 +1,37 @@
 import { useEffect } from 'react';
 
-const useDataFetcher = (selectedCategory, setGlobalData) => {
+/**
+ * A generic data fetching hook that can fetch data based on a key (e.g., selectedCategory)
+ * and update the global state. An optional updater function can be provided for custom update logic.
+ * 
+ * @param {string} key - The key to fetch data for (e.g., 'selectedCategory').
+ * @param {Function} setGlobalData - The state setter function for the global data.
+ * @param {Function} [updater] - Optional. Custom function to update the global state with fetched data.
+ */
+const useDataFetcher = (key, setGlobalData, updater) => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(`https://api.census.gov/data/2022/acs/acs5?get=B01001_001E&for=state%3A21&key=648f41eb22438d2f70b071d809f753df5c5c66e9`);
+        const response = await fetch(`https://api.census.gov/data/2022/acs/acs5?get=B01001_001E&for=state%3A21&key=${import.meta.env.VITE_APP_API_KEY}`);
         const data = await response.json();
-        // Update globalData with fetched data
-        setGlobalData(prevData => ({
-          ...prevData,
-          [selectedCategory]: [data],
-        }));
+        console.log("Fetched data for key:", key, data);
+        // Invoke updater function if provided, else update globalData directly
+        setGlobalData(prevData => {
+          const update = updater ? updater(prevData[key], data) : data;
+          return {
+            ...prevData,
+            [key]: update,
+          };
+        });
       } catch (error) {
-        console.error("Failed to fetch data for category:", selectedCategory, error);
+        console.error("Failed to fetch data for key:", key, error);
       }
     };
 
-    if (selectedCategory) {
+    if (key) {
       fetchData();
     }
-  }, [selectedCategory, setGlobalData]);
+  }, [key, setGlobalData, updater]); // Include updater in the dependency array
 };
 
 export default useDataFetcher;
