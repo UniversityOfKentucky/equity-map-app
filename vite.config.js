aee875e2bd64adc8fd452e8b964360682c94847c
Phillip <96359059+phillipashford@users.noreply.github.com>
2024-04-07 22:16:08 -0400

    Setup dotenv in Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { config } from 'dotenv'
 
-// https://vitejs.dev/config/
+config()
+
 export default defineConfig({
   plugins: [react()],
 })
