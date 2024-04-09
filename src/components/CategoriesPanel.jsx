ed074f259ffeeeaf90a903d06db378d732e63aa7
Phillip <96359059+phillipashford@users.noreply.github.com>
2024-04-08 23:04:34 -0400

    Update CategoriesPanel with dynamic fetch
    Enhanced CategoriesPanel to dynamically fetch data based on selected category. Implemented useState to manage endpoints and keys, refining data fetching.

--- src/components/CategoriesPanel.jsx
+++ src/components/CategoriesPanel.jsx
@@ -1,47 +1,49 @@
-import React, { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { Disclosure } from '@headlessui/react';
 import { ChevronUpIcon } from '@heroicons/react/24/outline';
 import { GlobalStateContext } from '../context/GlobalStateContext';
-import CategoryConfig from '../config/CategoryConfig';
+import Config from '../config/CategoryConfig';
 import useDataFetcher from '../hooks/useDataFetcher';
 
 const CategoriesPanel = () => {
-
-
-    // Destructures the undefined setSelectedCategory function from the GlobalStateContext object and assigns it to the setSelectedCategory variable.
-    const { selectedCategory, setSelectedCategory, globalData, setGlobalData } = useContext(GlobalStateContext);
-
-    // At the top level of your CategoriesPanel component
-    useDataFetcher(selectedCategory, setGlobalData);
-
-    // Handler to update the global state
-    const handleSelectCategory = (category) => {
-        setSelectedCategory(category);
+    const [endpointsAndKeys, setEndpointsAndKeys] = useState();
+    const { selectedCategory, setSelectedCategory, setGlobalData, globalData } = useContext(GlobalStateContext);
+  
+    const handleSelectCategory = (categoryKey) => {
+      setSelectedCategory(categoryKey);
+  
+      const category = Config.categories[categoryKey];
+      const newEndpointsAndKeys = category.visualization.map(visualization => ({
+        endpoint: visualization.endpoint,
+        key: `${visualization.title} ${visualization.type}`,
+      }));
+      setEndpointsAndKeys(newEndpointsAndKeys);
     };
+    useDataFetcher(endpointsAndKeys, setGlobalData);
     
     return (
         <div className="categories-panel col-span-1 h-screen overflow-auto">
-            {CategoryConfig.map((category) => (
-                <Disclosure key={category.title} as="div" className="m-2">
+            {Object.keys(Config.categories).map((categoryKey) => (
+                <Disclosure key={Config.categories[categoryKey].title} as="div" className="m-2">
                     {({ open }) => (
                         <>
                             <Disclosure.Button 
                                 className="flex justify-between w-full px-4 py-2 text-sm font-medium text-left text-blue-700 bg-blue-100 rounded-lg hover:bg-blue-200 focus:outline-none focus-visible:ring focus-visible:ring-blue-500 focus-visible:ring-opacity-75"
-                                onClick={() => handleSelectCategory(category.title)}
+                                onClick={() => handleSelectCategory(categoryKey)}
                             >
-                                {category.title}
+                                {categoryKey}
                                 <ChevronUpIcon
                                     className={`${open ? 'transform rotate-180' : ''} w-5 h-5 text-blue-500`}
                                 />
                             </Disclosure.Button>
                             <Disclosure.Panel className="m-2 px-4 pt-4 pb-2 text-sm text-gray-500 transition duration-500 ease-out">
                                 <div className="p-4 border border-gray-200 rounded-lg">
-                                    <p>Information about {category.title}</p>
+                                    <p>Information about {categoryKey}</p>
                                     {/*loops over visualizations in category object */}
-                                    {category.visualization.map((visualization) => (
+                                    {Config.categories[categoryKey].visualization.map((visualization) => (
                                         <div key={visualization.title} className="m-2">
                                             <p>{visualization.title}</p>
-                                            <p>{globalData[category.title]}</p>
+                                            <p>{globalData[categoryKey]}</p>
                                             <visualization.VisualizationComponent data={visualization.data} options={visualization.options} title={visualization.title}/>
                                         </div>
                                     ))}
