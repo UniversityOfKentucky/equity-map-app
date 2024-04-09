4edb2ea7b37f3311b09016814e74c64217f1a12f
Phillip <96359059+phillipashford@users.noreply.github.com>
2024-04-08 22:59:49 -0400

    Refactor MapView and add context
    Introduced useContext for global state management and PropTypes for prop validation. Removed useRef, added MapUpdater component for dynamic map settings adjustments based on context.

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -1,28 +1,77 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { MapContainer, TileLayer, GeoJSON, Popup } from 'react-leaflet'; 
+import { useState, useEffect, useContext } from 'react';
+import PropTypes from 'prop-types';
+import { MapContainer, TileLayer, GeoJSON, Popup, useMap } from 'react-leaflet'; 
 import 'leaflet/dist/leaflet.css';
+import { GlobalStateContext } from '../context/GlobalStateContext';
 import FeaturesPanel from './FeaturesPanel';
 
-const MapView = () => {
+function MapUpdater({ mapSettings }) {
+  const map = useMap();
 
-  const mapRef = useRef(null);
-
-  const position = [38, -84.5037];
-
-  // Array destructuring to set the state of 'geojsonData' and 'setGeojsonData' function
-  const [geojsonData, setGeojsonData] = useState(null);
-
-  // Fetches GeoJSON data from the local file 'fayette.geojson' and sets the 'setGeojsonData' state
   useEffect(() => {
-    fetch('src/assets/data/fayette-county.geojson')
+    const { center, zoom, minZoom, maxZoom } = mapSettings;
+    const maxBounds = [[center[0]*1.005, center[1]*1.005], [center[0]*.995, center[1]*.995]];
+    map.setView(center, zoom);
+    map.setMinZoom(minZoom);
+    map.setMaxZoom(maxZoom);
+    map.setMaxBounds(maxBounds);
+  }, [map, mapSettings]);
+
+  return null;
+}
+
+MapUpdater.propTypes = {
+  mapSettings: PropTypes.shape({
+    center: PropTypes.arrayOf(PropTypes.number).isRequired,
+    zoom: PropTypes.number.isRequired,
+    minZoom: PropTypes.number.isRequired,
+    maxZoom: PropTypes.number.isRequired,
+  }).isRequired,
+};
+
+const MapView = () => {
+  const { selectedCategory, selectedGeography } = useContext(GlobalStateContext);
+
+  const INITIAL_MAP_SETTINGS = {
+    center: [38, -84.5037],
+    zoom: 11,
+    minZoom: 11,
+    maxZoom: 19,
+  };
+ 
+  const [mapSettings, setMapSettings] = useState(INITIAL_MAP_SETTINGS);
+  const [geojsonData, setGeojsonData] = useState(null);
+  const INITIAL_GEOJSON_URL = 'src/assets/data/fayette-county.geojson';
+
+  useEffect(() => {
+    if (selectedGeography === 'msa') {
+      // Update mapSettings based on new geography
+      setMapSettings(/* new settings */);
+    }
+  }, [selectedGeography]);
+
+  useEffect(() => {
+    fetch(INITIAL_GEOJSON_URL)
       .then(response => response.json())
       .then(data => setGeojsonData(data))
       .catch(error => console.error(error));
   }, []);
 
+  useEffect(() => {
+    if (selectedCategory) {
+      // Fetch new GeoJSON data based on the selected category
+      const NEW_GEOJSON_URL = `src/assets/data/fayette-county.geojson`;
+      fetch(NEW_GEOJSON_URL)
+        .then(response => response.json())
+        .then(data => setGeojsonData(data))
+        .catch(error => console.error(error));
+    }
+  }, [selectedCategory]);
+
   return (
     <div className="col-span-4 relative h-screen">
-      <MapContainer center={position} zoom={11} style={{ height: '100%', width: '100%' }} minZoom={11} maxZoom={19} scrollWheelZoom={false} maxBounds={[[38.25, -84.75], [37.75, -84.25]]} ref={mapRef}>
+      <MapContainer {...mapSettings} style={{ height: '100%', width: '100%' }}>
+        <MapUpdater mapSettings={mapSettings} />
         <TileLayer
           url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attributions">CARTO</a>'
