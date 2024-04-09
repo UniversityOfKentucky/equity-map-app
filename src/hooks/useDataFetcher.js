57420e1d499b54e2e918dad6cf8f67df1aca806b
Phillip <96359059+phillipashford@users.noreply.github.com>
2024-04-08 23:02:28 -0400

    Streamline useDataFetcher
    Simplified useDataFetcher by allowing multiple endpoints and corresponding keys. Removed the updater function parameter, reducing complexity and enhancing usability. Error handling now includes the endpoint in the log for easier debugging

--- src/hooks/useDataFetcher.js
+++ src/hooks/useDataFetcher.js
@@ -1,37 +1,25 @@
 import { useEffect } from 'react';
 
-/**
- * A generic data fetching hook that can fetch data based on a key (e.g., selectedCategory)
- * and update the global state. An optional updater function can be provided for custom update logic.
- * 
- * @param {string} key - The key to fetch data for (e.g., 'selectedCategory').
- * @param {Function} setGlobalData - The state setter function for the global data.
- * @param {Function} [updater] - Optional. Custom function to update the global state with fetched data.
- */
-const useDataFetcher = (key, setGlobalData, updater) => {
+const useDataFetcher = (endpointsAndKeys, setGlobalData) => {
   useEffect(() => {
     const fetchData = async () => {
-      try {
-        const response = await fetch(`https://api.census.gov/data/2022/acs/acs5?get=B01001_001E&for=state%3A21&key=${import.meta.env.VITE_APP_API_KEY}`);
-        const data = await response.json();
-        console.log("Fetched data for key:", key, data);
-        // Invoke updater function if provided, else update globalData directly
-        setGlobalData(prevData => {
-          const update = updater ? updater(prevData[key], data) : data;
-          return {
+      const keys = Object.keys(endpointsAndKeys);
+      for (const key of keys) {
+        try {
+          const response = await fetch(endpointsAndKeys[key].endpoint);
+          const data = await response.json();
+          // Update the globalData state with the fetched data
+          setGlobalData(prevData => ({
             ...prevData,
-            [key]: update,
-          };
-        });
-      } catch (error) {
-        console.error("Failed to fetch data for key:", key, error);
+            [endpointsAndKeys[key].key]: data,
+          }));
+        } catch (error) {
+          console.error(`Failed to fetch data from endpoint: ${JSON.stringify(endpointsAndKeys[key].endpoint)}`, error);
+        }
       }
-    };
-
-    if (key) {
-      fetchData();
     }
-  }, [key, setGlobalData, updater]); // Include updater in the dependency array
-};
+    fetchData();
+  }, [endpointsAndKeys, setGlobalData]);
+}
 
 export default useDataFetcher;
