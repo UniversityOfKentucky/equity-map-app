0bfd881a6f5b0f971a4b7adeba5fa8ea3cd9db33
Phillip <96359059+phillipashford@users.noreply.github.com>
2024-04-26 18:55:17 -0400

    Enhanced useDataFetcher hook logic
    Updated useDataFetcher hook to include data transformation functionality using the new transformData utility, depending on the transformationType specified. It now fetches data using API keys and handles different geographies.

--- src/hooks/useDataFetcher.js
+++ src/hooks/useDataFetcher.js
@@ -1,25 +1,42 @@
 import { useEffect } from 'react';
+import { transformData } from '../utils/transform';
 
-const useDataFetcher = (endpointsAndKeys, setGlobalData) => {
+const useDataFetcher = (endpointsAndKeys, selectedGeography, setGlobalData) => {
   useEffect(() => {
     const fetchData = async () => {
       const keys = Object.keys(endpointsAndKeys);
       for (const key of keys) {
         try {
-          const response = await fetch(endpointsAndKeys[key].endpoint);
+          const response = await fetch(`${endpointsAndKeys[key].endpoint}&key=${import.meta.env.VITE_APP_API_KEY}`);
+
           const data = await response.json();
-          // Update the globalData state with the fetched data
-          setGlobalData(prevData => ({
-            ...prevData,
-            [endpointsAndKeys[key].key]: data,
-          }));
+          /////////////////////////////////////////
+          // if transformationType is defined, parse the data
+          if (endpointsAndKeys[key].transformationType) {
+            
+            // console.log(
+            //   "data: ", data,
+            //   " geography: ", selectedGeography,
+            //   " transformationType: ", endpointsAndKeys[key].transformationType);
+              // uses the useDataTransformer function to transform the data
+            const transformedData = transformData(data, selectedGeography, endpointsAndKeys[key].transformationType);
+            console.log("transformedData: ", transformedData);
+            // Update the globalData state with the parsed data
+            setGlobalData(prevData => ({
+              ...prevData,
+              [endpointsAndKeys[key].key]: transformedData,
+            }));
+          } else {
+            // If transformationType is not defined, store the data as is
+            console.error('No transformationType defined for endpoint:', endpointsAndKeys[key].endpoint);
+          }
         } catch (error) {
           console.error(`Failed to fetch data from endpoint: ${JSON.stringify(endpointsAndKeys[key].endpoint)}`, error);
         }
       }
     }
     fetchData();
-  }, [endpointsAndKeys, setGlobalData]);
+  }, [endpointsAndKeys, selectedGeography, setGlobalData]);
 }
 
 export default useDataFetcher;
