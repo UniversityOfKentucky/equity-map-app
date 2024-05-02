74cadb911e457146a176e9d9dda05e68ea7fa1a5
Phillip <96359059+phillipashford@users.noreply.github.com>
2024-05-02 18:21:39 -0400

    Refactor CategoriesPanel component
    Refactored CategoriesPanel for better state management and effect utilization. Simplified the component structure by removing unused code and centralizing context hooks and state operations. Enhanced functionality by using useEffect for updating endpointsAndKeys based on category and geography changes.

--- src/components/CategoriesPanel.jsx
+++ src/components/CategoriesPanel.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import { Disclosure, RadioGroup } from '@headlessui/react';
 import { ChevronUpIcon, CheckIcon } from '@heroicons/react/24/outline';
 import { GlobalStateContext } from '../context/GlobalStateContext';
@@ -13,40 +13,25 @@ const geographies = {
 
 const CategoriesPanel = () => {
 
-    const [endpointsAndKeys, setEndpointsAndKeys] = useState({
-                endpoint: `https://api.census.gov/data/2022/acs/acs5?get=B25077_001E&for=tract:*&in=state:21&in=county:067`,
-                key: `${Config.categories['Demographics'].title} Category Default Mapped Variable: ${Config.categories['Demographics'].defaultMappedVariable.label}`,
-                transformationType: Config.categories['Demographics'].defaultMappedVariable.transformationType
-            });
-            console.log('endpointsAndKeys:', endpointsAndKeys)
+  const {setSelectedCategory, selectedGeography, setGlobalData, selectedCategory, setSelectedGeography} = useContext(GlobalStateContext);
 
-    const {setSelectedCategory, selectedGeography, setGlobalData, selectedCategory, setSelectedGeography} = useContext(GlobalStateContext);
-  
-    const handleSelectCategory = (categoryKey) => {
-      setSelectedCategory(categoryKey);
-    
-    //     const category = Config.categories[categoryKey];
+  const [endpointsAndKeys, setEndpointsAndKeys] = useState({
+              endpoint: Config.categories[selectedCategory].defaultMappedVariable.endpoint[selectedGeography],
+              key: `${selectedGeography} - ${Config.categories[selectedCategory].title} Category Default Mapped Variable: ${Config.categories[selectedCategory].defaultMappedVariable.label}`,
+              transformationType: Config.categories[selectedCategory].defaultMappedVariable.transformationType
+          });
 
-    //     const newEndpointsAndKeys = [
-    //         {
-    //             endpoint: category.defaultMappedVariable.endpoint,
-    //             key: `${category.title} Category Default Mapped Variable: ${category.defaultMappedVariable.label}`,
-    //             transformationType: category.defaultMappedVariable.transformationType
-    //         },
-    //         // ...category.visualization.map(visualization => ({
-    //         //     endpoint: visualization.endpoint || "yet to be populated",
-    //         //     key: `${category.title} Category Visualization: ${visualization.title} (${visualization.type})`,
-    //         //     transformationType: visualization.transformationType || "yet to be populated",
-    //         //     dataFormat: visualization.sampleData || "yet to be populated"
-    //         // }))
-    //     ];
+  useEffect(() => {
+    setEndpointsAndKeys([
+      {
+      endpoint: Config.categories[selectedCategory].defaultMappedVariable.endpoint[selectedGeography],
+      key: `${selectedGeography} - ${Config.categories[selectedCategory].title} Category Default Mapped Variable: ${Config.categories[selectedCategory].defaultMappedVariable.label}`,
+      transformationType: Config.categories[selectedCategory].defaultMappedVariable.transformationType
+      }
+    ]);
+  }, [selectedCategory, selectedGeography]);
 
-    //     console.log('newEndpointsAndKeys:', newEndpointsAndKeys)
-
-    //   setEndpointsAndKeys(newEndpointsAndKeys);
-    };
-
-    // useDataFetcher(endpointsAndKeys, selectedGeography, setGlobalData, selectedCategory);
+    useDataFetcher(endpointsAndKeys, selectedGeography, setGlobalData);
     
     return (
       <div className="categories-panel col-span-1 h-screen overflow-auto">
@@ -70,24 +55,21 @@ const CategoriesPanel = () => {
 </RadioGroup>
         {Object.keys(Config.categories).map((categoryKey) => (
           <Disclosure
-            key={Config.categories[categoryKey].title}
+            key={categoryKey}
             as="div"
             className="m-2"
           >
             {({ open }) => (
               <>
-                <Disclosure.Button
-                  // key={geojsonData}
-                  className="flex justify-between w-full px-4 py-2 text-sm font-medium text-left text-blue-700 bg-blue-100 rounded-lg hover:bg-blue-200 focus:outline-none focus-visible:ring focus-visible:ring-blue-500 focus-visible:ring-opacity-75"
-                  onClick={() => handleSelectCategory(categoryKey)}
-                >
-                  {categoryKey}
-                  <ChevronUpIcon
-                    className={`${
-                      open ? "transform rotate-180" : ""
-                    } w-5 h-5 text-blue-500`}
-                  />
-                </Disclosure.Button>
+            <Disclosure.Button
+              className="flex justify-between w-full px-4 py-2 text-sm font-medium text-left text-blue-700 bg-blue-100 rounded-lg hover:bg-blue-200 focus:outline-none focus-visible:ring focus-visible:ring-blue-500 focus-visible:ring-opacity-75"
+              onClick={() => setSelectedCategory(categoryKey)}
+            >
+              {categoryKey}
+              <ChevronUpIcon
+                className={`${open ? "transform rotate-180" : ""} w-5 h-5 text-blue-500`}
+              />
+            </Disclosure.Button>
                 <Disclosure.Panel className="m-2 px-4 pt-4 pb-2 text-sm text-gray-500 transition duration-500 ease-out">
                   <div className="p-4 border border-gray-200 rounded-lg">
                     <p>{Config.categories[categoryKey].overview}</p>
