4aac8bdb5cedcee1e9645ed5844bf0ad69417222
Phillip <96359059+phillipashford@users.noreply.github.com>
2024-05-02 18:26:28 -0400

    Simplify and optimize MapView component
    Removed unused PropTypes and MapUpdater component, streamlined GeoJSON data fetching and display logic using new context and hooks. Improved readability and maintainability, and enhanced data integration with dynamic coloring based on transformed data values.

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -1,88 +1,83 @@
+// MapView.jsx
 import { useState, useEffect, useContext } from 'react';
-import PropTypes from 'prop-types';
-import { MapContainer, TileLayer, GeoJSON, Popup, useMap } from 'react-leaflet'; 
+import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet'; 
 import 'leaflet/dist/leaflet.css';
 import { GlobalStateContext } from '../context/GlobalStateContext';
 import FeaturesPanel from './FeaturesPanel';
-
-function MapUpdater({ mapSettings }) {
-  const map = useMap();
-
-  useEffect(() => {
-    const { center, zoom, minZoom, maxZoom } = mapSettings;
-    const maxBounds = [[center[0]*1.005, center[1]*1.005], [center[0]*.995, center[1]*.995]];
-    map.setView(center, zoom);
-    map.setMinZoom(minZoom);
-    map.setMaxZoom(maxZoom);
-    map.setMaxBounds(maxBounds);
-  }, [map, mapSettings]);
-
-  return null;
-}
-
-MapUpdater.propTypes = {
-  mapSettings: PropTypes.shape({
-    center: PropTypes.arrayOf(PropTypes.number).isRequired,
-    zoom: PropTypes.number.isRequired,
-    minZoom: PropTypes.number.isRequired,
-    maxZoom: PropTypes.number.isRequired,
-  }).isRequired,
-};
+import Config from '../config/CategoryConfig';
+import chroma from 'chroma-js';
+import { useMapSettings } from '../hooks/useMapSettings';
 
 const MapView = () => {
-  const { selectedCategory, selectedGeography } = useContext(GlobalStateContext);
-
-  const INITIAL_MAP_SETTINGS = {
-    center: [38, -84.5037],
-    zoom: 11,
-    minZoom: 11,
-    maxZoom: 19,
-  };
- 
-  const [mapSettings, setMapSettings] = useState(INITIAL_MAP_SETTINGS);
-  const [geojsonData, setGeojsonData] = useState(null);
-  const INITIAL_GEOJSON_URL = 'src/assets/data/fayette-county.geojson';
+  const { selectedCategory, selectedGeography, globalData, geojsonData, setGeojsonData } = useContext(GlobalStateContext);
+  const mapSettings = useMapSettings(selectedGeography);
+  const [geojsonKey, setGeojsonKey] = useState(Date.now());
 
   useEffect(() => {
-    if (selectedGeography === 'msa') {
-      // Update mapSettings based on new geography
-      setMapSettings(/* new settings */);
+    async function fetchGeojson() {
+      const response = await fetch(`src/assets/data/${selectedGeography}.geojson`);
+      const data = await response.json();
+      const transformedData = globalData[`${selectedGeography} - ${selectedCategory} Category Default Mapped Variable: ${Config.categories[selectedCategory].defaultMappedVariable.label}`];
+      data.features.forEach(feature => {
+        const geoCode = feature.properties[{ 'tracts': 'TRACTCE', 'counties': 'COUNTYFP', 'msa': 'GEOID' }[selectedGeography]];
+        let value = transformedData[geoCode];
+        if (value < 0 || isNaN(value) || value === undefined) value = null;
+        feature.properties.variableValue = value;
+      });
+      const colorScale = chroma.scale(['rgb(219, 234, 254)', 'rgb(59, 130, 246)']).mode('lch').colors(6);
+      const values = data.features.map(f => f.properties.variableValue);
+      const min = Math.min(...values);
+      const max = Math.max(...values);
+      const normalize = (value, min, max) => ((value - min) / (max - min)) * (colorScale.length - 1);
+      setGeojsonData({ colorScale, data, min, max, normalize });
+      setGeojsonKey(Date.now());
     }
-  }, [selectedGeography]);
-
-  useEffect(() => {
-    fetch(INITIAL_GEOJSON_URL)
-      .then(response => response.json())
-      .then(data => setGeojsonData(data))
-      .catch(error => console.error(error));
-  }, []);
-
-  useEffect(() => {
-    if (selectedCategory) {
-      // Fetch new GeoJSON data based on the selected category
-      const NEW_GEOJSON_URL = `src/assets/data/fayette-county.geojson`;
-      fetch(NEW_GEOJSON_URL)
-        .then(response => response.json())
-        .then(data => setGeojsonData(data))
-        .catch(error => console.error(error));
-    }
-  }, [selectedCategory]);
+    fetchGeojson();
+  }, [globalData, selectedCategory, selectedGeography, setGeojsonData]);
 
   return (
-    <div className="col-span-4 relative h-screen">
-      <MapContainer {...mapSettings} style={{ height: '100%', width: '100%' }}>
-        <MapUpdater mapSettings={mapSettings} />
+    <div className="col-span-3 relative h-screen">
+      <MapContainer key={`${selectedCategory}-${selectedGeography}`} {...mapSettings} style={{ height: '100%', width: '100%' }}>
         <TileLayer
           url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attributions">CARTO</a>'
-          subdomains='abcd'
-          maxZoom= '19'
+          maxZoom='19'
         />
-        {/* If geojosnData is available, render the GeoJSON component with the data */}
         {geojsonData && (
-          <GeoJSON data={geojsonData}>
-            <Popup>A sample Popup.</Popup>
-          </GeoJSON>
+          <GeoJSON
+            key={geojsonKey}
+            data={geojsonData.data}
+            style={(feature) => ({
+              fillColor: geojsonData.colorScale[Math.floor(geojsonData.normalize(feature.properties.variableValue, geojsonData.min, geojsonData.max))],
+              weight: 2,
+              opacity: 1,
+              color: 'white',
+              fillOpacity: 0.7
+            })}
+            onEachFeature={(feature, layer) => {
+              if (feature.properties && feature.properties.variableValue) {
+                let nameLabelString = `<b>${feature.properties.NAMELSAD}</b><br> ${Config.categories[selectedCategory].defaultMappedVariable.label}:`;
+                let value = feature.properties.variableValue;
+                switch (Config.categories[selectedCategory].defaultMappedVariable.transformationType) {
+                  case 'percentage':
+                  case 'summedPercentage':
+                    layer.bindPopup(`${nameLabelString} ${value}%`);
+                    break;
+                  case 'convertToDollars':
+                    layer.bindPopup(`${nameLabelString} $${value.toLocaleString()}`);
+                    break;
+                  case 'ratePerThousand':
+                    layer.bindPopup(`${nameLabelString} ${value}/1,000`);
+                    break;
+                  case 'none':
+                    layer.bindPopup(`${nameLabelString} ${value.toLocaleString()}`);
+                    break;
+                }
+              } else {
+                layer.bindPopup(`${feature.properties.NAMELSAD}: No data available`);
+              }
+            }}
+          />
         )}
       </MapContainer>
       <FeaturesPanel />
