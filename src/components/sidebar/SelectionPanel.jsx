2effda3de0ae8240d19d6a77c39fdfb4cf230706
Phillip <96359059+phillipashford@users.noreply.github.com>
2024-05-09 23:14:13 -0400

    Create SelectionPanel component
    Implemented SelectionPanel component to consolidate geography and category selectors. This centralizes the selection controls, streamlining user interactions.

---
+++ src/components/sidebar/SelectionPanel.jsx
@@ -0,0 +1,37 @@
+import GeographySelector from './GeographySelector';
+// import TimePeriodSelector from './TimePeriodSelector';
+import CategorySelector from './CategorySelector';
+// import FeaturesPanel from './FeaturesPanel.jsx';
+import propTypes from 'prop-types';
+
+const SelectionPanel = ({ selectedGeography, setSelectedGeography, selectedVariable, setSelectedVariable }) => {
+
+    return (
+        <div className="selection-panel col-span-1 h-screen overflow-auto">
+            <GeographySelector 
+                classname=""
+                selectedGeography={selectedGeography}
+                setSelectedGeography={setSelectedGeography}
+            />
+            {/* <TimePeriodSelector /> */}
+            <CategorySelector 
+                classname=""
+                selectedVariable={selectedVariable}
+                setSelectedVariable={setSelectedVariable}
+            />
+            <div className="m-2 border border-gray-200 rounded-lg">
+                <p>Information about Project</p>
+                {/* <FeaturesPanel /> */}
+            </div>
+        </div>
+    );
+}
+
+SelectionPanel.propTypes = {
+    selectedGeography: propTypes.string.isRequired,
+    setSelectedGeography: propTypes.func.isRequired,
+    selectedVariable: propTypes.string.isRequired,
+    setSelectedVariable: propTypes.func.isRequired,
+}
+
+export default SelectionPanel;
