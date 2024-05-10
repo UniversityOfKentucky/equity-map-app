02450867f23afcdbe1cd1e18f0fa81000da4ad8d
Phillip <96359059+phillipashford@users.noreply.github.com>
2024-05-09 23:15:40 -0400

    Simplify GlobalStateContext structure
    Streamlined the GlobalStateContext by removing unnecessary state and focusing on essential data. This simplifies state management and aligns with a data-focused refactor strategy.

--- src/context/GlobalStateContext.jsx
+++ src/context/GlobalStateContext.jsx
@@ -1,41 +1,26 @@
 import { createContext, useState } from 'react';
 import PropTypes from 'prop-types';
-import { initFlatFileData } from '../assets/data/initFlatFileData.js';
 
 // Creates a context object to store the shared state variables.
-export const GlobalStateContext = createContext();
+const GlobalStateContext = createContext();
 
-// Once imported into main.jsx, the GlobalStateProvider component will wrap main.jsx's child components, allowing them to access the shared state variables.
-export const GlobalStateProvider = ({ children }) => { // This function manages the shared state variables and returns their values to the children components via props.
-
-    // Shared state variables and their initial values
-    const [selectedCategory, setSelectedCategory] = useState('Demographics');
-    const [globalData, setGlobalData] = useState(initFlatFileData); // Temporarily initializes from the flat file data for development
-    const [selectedGeography, setSelectedGeography] = useState('tracts');
-    const [geojsonData, setGeojsonData] = useState();
-
-    // Value to be passed to provider consumers
-    const value = {
-        selectedCategory,
-        setSelectedCategory,
-        globalData,
-        setGlobalData,
-        selectedGeography,
-        setSelectedGeography,
-        geojsonData,
-        setGeojsonData,
-    };
+export const GlobalStateProvider = ({ children }) => {
+    
+    const [globalData, setGlobalData] = useState({ geoJsonData: {}, apiData: {}, transformedData: {} });
 
     return (
-        // React's Context API provider component
-        <GlobalStateContext.Provider value={value}>
+        <GlobalStateContext.Provider value={{
+            globalData,
+            setGlobalData 
+            }}>
             {children}
         </GlobalStateContext.Provider>
     );
 };
 
-// Prop types validation
 GlobalStateProvider.propTypes = {
     children: PropTypes.node.isRequired,
 };
+
+export default GlobalStateContext;
 
