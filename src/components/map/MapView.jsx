eb5d8524dd2c0db92dcc0e2885f204eb0fec98bf
Phillip <96359059+phillipashford@users.noreply.github.com>
2024-05-14 22:40:06 -0400

    Integrate data fetching in MapView
    Refactored MapView to use React Query for data fetching and state management. This approach not only enhances performance by reducing unnecessary re-renders but also keeps the data fetching and UI in sync.

--- src/components/map/MapView.jsx
+++ src/components/map/MapView.jsx
@@ -1,44 +1,66 @@
-import { MapContainer, TileLayer } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
+import { useEffect, useState } from "react";
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 import PropTypes from "prop-types";
-import { appConfig } from '../../config/config';
-import DataLayerContainer from './DataLayerContainer';
+import { appConfig } from "../../config/config";
+import DataLayerContainer from "./DataLayerContainer";
+import constructFetchURL from "../../utils/constructFetchURL";
+import { processData } from "../../utils/dataProcessingUtils.js";
+import { useQuery } from 'react-query';
 
 const MapView = ({ selectedGeography, selectedVariable }) => {
+  const [processedData, setProcessedData] = useState(null);
+
+  useQuery(
+    ['censusData', selectedGeography, selectedVariable],
+    () => fetch(constructFetchURL(selectedGeography, selectedVariable)).then(res => res.json()),
+    {
+      enabled: selectedVariable !== "No current selection" && !!selectedGeography,
+      onSuccess: (data) => {
+        const processed = processData(data, selectedVariable, selectedGeography);
+        setProcessedData(processed);
+      },
+      onError: (error) => {
+        console.error("Failed to fetch data: ", error);
+      }
+    }
+  );
+
+  useEffect(() => {
+    // Reset processed data when geography changes
+    setProcessedData(null);
+  }, [selectedGeography]);
 
   const mapSettings = appConfig.geographies[selectedGeography].mapSettings;
   const center = mapSettings.center;
-
   const maxBounds = [
-    [center[0] * 1.005, center[1] * 1.005], // top left corner
-    [center[0] * 0.995, center[1] * 0.995]  // bottom right corner
+    [center[0] * 1.005, center[1] * 1.005],
+    [center[0] * 0.995, center[1] * 0.995],
   ];
   mapSettings.maxBounds = maxBounds;
 
   return (
-
     <div className="col-span-3 relative h-screen">
-      {/* Add property to prevent map from animating and 'bouncing' back when attempting to escape bounds */}
-      <MapContainer 
-        key={`${selectedVariable}-${selectedGeography}`} 
+      <MapContainer
+        key={`${selectedVariable}-${selectedGeography}`}
         maxBoundsViscosity={1.0}
-        {...appConfig.geographies[selectedGeography].mapSettings} 
-        style={{ height: '100%', width: '100%'}}
-      > 
+        {...mapSettings}
+        style={{ height: "100%", width: "100%" }}
+      >
         <TileLayer
           url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attributions">CARTO</a>'
-          maxZoom='19'
+          maxZoom="19"
         />
-        <DataLayerContainer 
-          selectedGeography={selectedGeography} 
+        <DataLayerContainer
+          processedData={processedData}
+          selectedGeography={selectedGeography}
           selectedVariable={selectedVariable}
         />
       </MapContainer>
-      {/* <FeaturesPanel /> */}
     </div>
   );
-}
+};
 
 MapView.propTypes = {
   selectedGeography: PropTypes.string.isRequired,
