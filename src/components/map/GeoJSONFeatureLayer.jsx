02db6725363994526db8b1c0937df08d96aa0828
Phillip <96359059+phillipashford@users.noreply.github.com>
2024-05-14 22:41:18 -0400

    Dynamic styling for GeoJSON layers
    Updated GeoJSONFeatureLayer to dynamically apply styles based on the feature properties

--- src/components/map/GeoJSONFeatureLayer.jsx
+++ src/components/map/GeoJSONFeatureLayer.jsx
@@ -4,8 +4,8 @@ import PropTypes from 'prop-types';
 
 const GeoJSONFeatureLayer = ({ data, selectedGeography, selectedVariable }) => {
   const memoizedOptions = useMemo(() => {
-    const style = () => ({
-      fillColor: 'rgb(29, 78, 216)',
+    const style = (feature) => ({
+      fillColor: feature.properties.color || 'rgb(29, 78, 216)',
       weight: 2,
       opacity: 1,
       color: 'white',
@@ -20,7 +20,8 @@ const GeoJSONFeatureLayer = ({ data, selectedGeography, selectedVariable }) => {
     return { style, onEachFeature };
   }, [selectedVariable]);
 
-  return <GeoJSON key={selectedGeography} data={data} {...memoizedOptions} />;
+  return data ? <GeoJSON key={selectedGeography} data={data} {...memoizedOptions} /> : null;
+
 };
 
 GeoJSONFeatureLayer.propTypes = {
@@ -30,130 +31,3 @@ GeoJSONFeatureLayer.propTypes = {
 };
 
 export default GeoJSONFeatureLayer;
-
-// import { useMemo } from 'react';
-// import { GeoJSON } from 'react-leaflet';
-// import PropTypes from 'prop-types';
-
-// // Purpose:
-// // - Display GeoJSON data with consistent styling and popups.
-// // - Memoize styles and event handlers for optimal rendering.
-
-// const GeoJSONFeatureLayer = ({ data, selectedGeography, selectedVariable }) => {
-//   // Define consistent styles for the GeoJSON features
-//   const style = () => ({
-//     fillColor: 'rgb(29, 78, 216)',
-//     weight: 2,
-//     opacity: 1,
-//     color: 'white',
-//     fillOpacity: 0.7
-//   });
-
-//   // Event handler to bind popups to features, using selected variable
-//   const onEachFeature = (feature, layer) => {
-//     const popupContent = `<div><h2>${feature.properties.NAMELSAD}</h2><p>${selectedVariable}</p></div>`;
-//     layer.bindPopup(popupContent);
-//   };
-
-//   // Memoize GeoJSON options for efficient rendering
-//   const memoizedOptions = useMemo(() => ({
-//     style: style,
-//     onEachFeature: onEachFeature
-//   }), [selectedVariable]);
-
-//   return <GeoJSON key={selectedGeography} data={data} {...memoizedOptions} />;
-// };
-
-// GeoJSONFeatureLayer.propTypes = {
-//   data: PropTypes.object.isRequired,
-//   selectedGeography: PropTypes.string.isRequired,
-//   selectedVariable: PropTypes.string.isRequired
-// };
-
-// export default GeoJSONFeatureLayer;
-
-
-// import { useState, useEffect } from 'react';
-// import { GeoJSON } from 'react-leaflet';
-// import { appConfig } from '../../config/config';
-// import PropTypes from "prop-types";
-
-// // The GeoJSONFeatureLayer should focus purely on rendering and not handle fetching directly.
-
-// // Purpose:
-//   // Display a specific GeoJSON layer on the map with consistent styling and popups.
-// // Responsibilities:
-//   // Accept processed GeoJSON data and selected variable.
-//   // Bind popups or other interactivity to the features.
-//   // Style each feature based on data values and normalization.
-
-// const GeoJSONFeatureLayer = ({ selectedGeography, selectedVariable }) => {
-//   const [geojsonData, setGeojsonData] = useState(null);
-//   const [layerKey, setLayerKey] = useState('');
-
-//   useEffect(() => {
-//     async function fetchGeojson() {
-//         const response = await fetch(`data/${appConfig.geographies[selectedGeography].geoJSONfileName}`);
-//       const data = await response.json();
-//       setGeojsonData(data);
-//       setLayerKey(`${selectedVariable}-${selectedGeography}-${Date.now()}`);
-//     }
-//     fetchGeojson();
-//   }, [selectedGeography, selectedVariable]);
-
-//   return (
-//     geojsonData && (
-//       <GeoJSON
-//         key={layerKey}
-//         data={geojsonData}
-//         style={() => ({
-//           fillColor: 'rgb(29, 78, 216)',
-//           weight: 2,
-//           opacity: 1,
-//           color: 'white',
-//           fillOpacity: 0.7
-//         })}
-//         onEachFeature={(feature, layer) => {
-//           const popupContent = `<div><h2>${feature.properties.NAMELSAD}</h2><p>${selectedVariable}</p></div>`;
-//           layer.bindPopup(popupContent);
-//         }}
-
-//         // onEachFeature={(feature, layer) => {
-//         //   if (feature.properties && feature.properties.variableValue !== undefined) {
-//         //     let nameLabelString = `<b>${feature.properties.NAMELSAD}</b><br> ${Config.categories[selectedVariable].defaultMappedVariable.label}:`;
-//         //     let value = feature.properties.variableValue;
-//         //     let popupContent = '';
-        
-//         //     switch (Config.categories[selectedVariable].defaultMappedVariable.transformationType) {
-//         //       case 'percentage':
-//         //       case 'summedPercentage':
-//         //         popupContent = `${nameLabelString} ${value}%`;
-//         //         break;
-//         //       case 'convertToDollars':
-//         //         popupContent = `${nameLabelString} $${value.toLocaleString()}`;
-//         //         break;
-//         //       case 'ratePerThousand':
-//         //         popupContent = `${nameLabelString} ${value}/1,000`;
-//         //         break;
-//         //       case 'none':
-//         //         popupContent = `${nameLabelString} ${value.toLocaleString()}`;
-//         //         break;
-//         //     }
-//         //     layer.bindPopup(popupContent);
-//         //   } else {
-//         //     layer.bindPopup(`${feature.properties.NAMELSAD}: No data available`);
-//         //   }
-//         // }}
-        
-
-//       />
-//     )
-//   );
-// };
-
-// GeoJSONFeatureLayer.propTypes = {
-//     selectedGeography: PropTypes.string,
-//     selectedVariable: PropTypes.string
-//     };
-
-// export default GeoJSONFeatureLayer;
