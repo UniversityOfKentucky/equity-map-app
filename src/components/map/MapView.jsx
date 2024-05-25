2ff1f4a3c2e73958eda6f461ba07b1da2ce3d30f
Phillip <96359059+phillipashford@users.noreply.github.com>
2024-05-25 09:09:38 -0400

    Simplify MapView component
    Removed redundant state and side effects. Adjusted map bounds and key properties for better rendering. Integrated changes from DataLayerContainer.

--- src/components/map/MapView.jsx
+++ src/components/map/MapView.jsx
@@ -1,48 +1,23 @@
-import { useEffect, useState } from "react";
 import { MapContainer, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import PropTypes from "prop-types";
 import { appConfig } from "../../config/config";
 import DataLayerContainer from "./DataLayerContainer";
-import constructFetchURL from "../../utils/constructFetchURL";
-import { processData } from "../../utils/dataProcessingUtils.js";
-import { useQuery } from 'react-query';
 
 const MapView = ({ selectedGeography, selectedVariable }) => {
-  const [processedData, setProcessedData] = useState(null);
-
-  useQuery(
-    ['censusData', selectedGeography, selectedVariable],
-    () => fetch(constructFetchURL(selectedGeography, selectedVariable)).then(res => res.json()),
-    {
-      enabled: selectedVariable !== "No current selection" && !!selectedGeography,
-      onSuccess: (data) => {
-        const processed = processData(data, selectedVariable, selectedGeography);
-        setProcessedData(processed);
-      },
-      onError: (error) => {
-        console.error("Failed to fetch data: ", error);
-      }
-    }
-  );
-
-  useEffect(() => {
-    // Reset processed data when geography changes
-    setProcessedData(null);
-  }, [selectedGeography]);
-
   const mapSettings = appConfig.geographies[selectedGeography].mapSettings;
   const center = mapSettings.center;
+
   const maxBounds = [
-    [center[0] * 1.005, center[1] * 1.005],
-    [center[0] * 0.995, center[1] * 0.995],
+    [center[0] * 1.1, center[1] * 1.1],
+    [center[0] * 0.9, center[1] * 0.9],
   ];
   mapSettings.maxBounds = maxBounds;
 
   return (
     <div className="col-span-3 relative h-screen">
       <MapContainer
-        key={`${selectedVariable}-${selectedGeography}`}
+        key={selectedGeography}
         maxBoundsViscosity={1.0}
         {...mapSettings}
         style={{ height: "100%", width: "100%" }}
@@ -53,7 +28,6 @@ const MapView = ({ selectedGeography, selectedVariable }) => {
           maxZoom="19"
         />
         <DataLayerContainer
-          processedData={processedData}
           selectedGeography={selectedGeography}
           selectedVariable={selectedVariable}
         />
