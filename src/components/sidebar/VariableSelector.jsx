5e54ee77903e1eec1a3676ff7cc18c147cdacd76
Phillip <96359059+phillipashford@users.noreply.github.com>
2024-05-25 09:43:35 -0400

    Improve VariableSelector UI for unavailable variables
    Updated VariableSelector to only display subcategories if they have one or more variables which are available for the selected geography.

--- src/components/sidebar/VariableSelector.jsx
+++ src/components/sidebar/VariableSelector.jsx
@@ -18,17 +18,18 @@ const VariableSelector = ({
   setSelectedVariable,
   selectedGeography,
 }) => {
-
   useMemo(() => {
-    referenceData.variables = generateVariablesReference(referenceData.categories);
-  }, []); // This will run once when the component is mounted
+    referenceData.variables = generateVariablesReference(
+      referenceData.categories
+    );
+  }, []);
 
   const isGeographyAvailable = (variableKey) => {
     const variableDataset =
       referenceData.variables[variableKey].dataset.displayedDataset;
-    const availableGeographies = referenceData.censusDataAPIs[variableDataset]
-      .geographiesAvailable;
-      let selectedGeosType =
+    const availableGeographies =
+      referenceData.censusDataAPIs[variableDataset].geographiesAvailable;
+    let selectedGeosType =
       appConfig.geographies[selectedGeography].typeOfGeography;
     return availableGeographies.includes(selectedGeosType);
   };
@@ -58,40 +59,66 @@ const VariableSelector = ({
                     {/* Loop over the subcategories in the category object*/}
                     {Object.keys(
                       referenceData.categories[categoryKey].data.subcategories
-                    ).map((subcategoryKey) => (
-                      <div key={subcategoryKey} className="m-2">
-                        <h2 className="font-bold text-lg">{subcategoryKey}</h2>
-                        <RadioGroup
-                          value={selectedVariable}
-                          onChange={setSelectedVariable}
-                          className="bg-blue-100 p-4 rounded-lg shadow m-2 mb-4"
-                        >
-                          {/* Loop over the variables in the subcategory object creating an element for each which will hold the selection element for the variable */}
-                          {Object.keys(
-                            referenceData.categories[categoryKey].data
-                              .subcategories[subcategoryKey]
-                          ).map((variableKey) => 
-                            isGeographyAvailable(variableKey) ? (
-                            <Field
-                              key={variableKey}
-                              className="flex items-stretch gap-2 self-start"
-                            >
-                              <Radio
-                                value={variableKey}
-                                className="group flex size-5 items-center justify-center rounded-full border bg-white data-[checked]:bg-blue-400"
-                              >
-                                <span className="invisible size-2 rounded-full bg-white group-data-[checked]:visible flex-wrap" />
-                              </Radio>
-                              <Label className="text-left">{variableKey}</Label>
-                              {/* <Description className="opacity-50">{variableKey.description}</Description>
+                    ).map((subcategoryKey) =>
+                      Object.keys(
+                        referenceData.categories[categoryKey].data
+                          .subcategories[subcategoryKey]
+                      ).some((variableKey) =>
+                        isGeographyAvailable(variableKey)
+                      ) ? (
+                        <div key={subcategoryKey} className="m-2">
+                          <h2 className="font-bold text-lg">
+                            {subcategoryKey}
+                          </h2>
+                          <RadioGroup
+                            value={selectedVariable}
+                            onChange={setSelectedVariable}
+                            className="bg-blue-100 p-4 rounded-lg shadow m-2 mb-4"
+                          >
+                            {/* Loop over the variables in the subcategory object creating an element for each which will hold the selection element for the variable */}
+                            {Object.keys(
+                              referenceData.categories[categoryKey].data
+                                .subcategories[subcategoryKey]
+                            ).map(
+                              (variableKey) =>
+                                isGeographyAvailable(variableKey) ? (
+                                  <Field
+                                    key={variableKey}
+                                    className="flex items-stretch gap-2 self-start"
+                                  >
+                                    <Radio
+                                      value={variableKey}
+                                      className="group flex size-5 items-center justify-center rounded-full border bg-white data-[checked]:bg-blue-400"
+                                    >
+                                      <span className="invisible size-2 rounded-full bg-white group-data-[checked]:visible flex-wrap" />
+                                    </Radio>
+                                    <Label className="text-left">
+                                      {variableKey}
+                                    </Label>
+                                    {/* <Description className="opacity-50">{variableKey.description}</Description>
                               // To be added to the config file for each variable to provide a description of the variable to the user
                               */}
-                            </Field>
-                            ) : null
-                          )}
-                        </RadioGroup>
-                      </div>
-                    ))}
+                                  </Field>
+                                ) : null
+                              // (
+                              // <Field key={variableKey} className="flex items-stretch gap-2 self-start">
+                              //   <Radio
+                              //     value={variableKey}
+                              //     className="group flex size-5 items-center justify-center rounded-full border bg-white data-[checked]:bg-blue-400"
+                              //     disabled
+                              //   >
+                              //     <span className="invisible size-2 rounded-full bg-white group-data-[checked]:visible flex-wrap" />
+                              //   </Radio>
+                              //   <Label className="text-left opacity-50" title="This variable is not available for the selected geography">
+                              //     {variableKey}
+                              //   </Label>
+                              // </Field>
+                              // )
+                            )}
+                          </RadioGroup>
+                        </div>
+                      ) : null
+                    )}
                   </div>
                 </DisclosurePanel>
               </>
