db24f9572da3dcf8b1299b66d7fc3950f1b0f3eb
Phillip <96359059+phillipashford@users.noreply.github.com>
2024-05-25 09:17:46 -0400

    Update GeoJSONFeatureLayer with dynamic popups
    Modified to display dynamic popup content based on the selected variable and its annotations. Improved styling and performance by using React.memo and removing unnecessary useMemo.

--- src/components/map/GeoJSONFeatureLayer.jsx
+++ src/components/map/GeoJSONFeatureLayer.jsx
@@ -1,33 +1,62 @@
-import { useMemo } from 'react';
-import { GeoJSON } from 'react-leaflet';
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
+import { GeoJSON } from "react-leaflet";
+import React from "react";
+import { appConfig, referenceData } from "../../config/config";
+import generateVariablesReference from "../../utils/generateVariables";
 
-const GeoJSONFeatureLayer = ({ data, selectedGeography, selectedVariable }) => {
-  const memoizedOptions = useMemo(() => {
-    const style = (feature) => ({
-      fillColor: feature.properties.color || 'rgb(29, 78, 216)',
-      weight: 2,
-      opacity: 1,
-      color: 'white',
-      fillOpacity: 0.7
-    });
 
-    const onEachFeature = (feature, layer) => {
-      const popupContent = `<div><h2>${feature.properties.NAMELSAD}</h2><p>${selectedVariable}</p></div>`;
-      layer.bindPopup(popupContent);
-    };
+const annotationValues = referenceData.annotationValues;
 
-    return { style, onEachFeature };
-  }, [selectedVariable]);
+referenceData.variables = generateVariablesReference(referenceData.categories);
 
-  return data ? <GeoJSON key={selectedGeography} data={data} {...memoizedOptions} /> : null;
+console.log('referenceData.variables: ', referenceData.variables)
 
+
+const GeoJSONFeatureLayer = ({ data, selectedVariable }) => {
+
+  const selectedDataset = referenceData.variables[selectedVariable].dataset.displayedDataset;
+
+  const style = (feature) => ({
+    fillColor: annotationValues[feature.properties.formattedData] ? "lightgrey" : feature.properties.color || "lightgrey",
+    weight: 2,
+    opacity: 1,
+    color: "white",
+    fillOpacity: 0.7,
+  });
+
+  const onEachFeature = (feature, layer) => {
+    const formattedData = feature.properties.formattedData;
+const popupContent =
+      annotationValues[formattedData] ? (
+        `<div>
+          <h2 class="text-xl">${feature.properties.NAMELSAD}</h2>
+          <h2>${selectedVariable}: <b>${annotationValues[formattedData].annotation}</b></h2>
+          <p>${annotationValues[formattedData].meaning}</p>
+          <p>Source: US Census ${referenceData.censusDataAPIs.datasetName} Dataset | ${appConfig.initialTimePeriod}</p>
+        </div>`
+      ) : (
+        `<div>
+        <h2 class="text-xl">${feature.properties.NAMELSAD}</h2>
+        <h2>${selectedVariable}: <b>${formattedData ? formattedData : "No Data"}</b></h2>
+          <p>Source: US Census Bureau's <a href="${referenceData.censusDataAPIs[selectedDataset].source}">${referenceData.censusDataAPIs[selectedDataset].datasetName} Dataset</a> | ${appConfig.initialTimePeriod}</p>
+          </div>`
+      );
+    layer.bindPopup(popupContent);
+  };
+
+  return (
+    <GeoJSON
+      key={`${data.features[0].properties[selectedVariable]}-${selectedVariable}`}
+      data={data}
+      style={style}
+      onEachFeature={onEachFeature}
+    />
+  );
 };
 
 GeoJSONFeatureLayer.propTypes = {
   data: PropTypes.object.isRequired,
-  selectedGeography: PropTypes.string.isRequired,
-  selectedVariable: PropTypes.string.isRequired
+  selectedVariable: PropTypes.string.isRequired,
 };
 
-export default GeoJSONFeatureLayer;
+export default React.memo(GeoJSONFeatureLayer);
