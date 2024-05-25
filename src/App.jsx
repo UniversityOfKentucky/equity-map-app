a7ed97c6049514fc225941662271d260381e9d24
Phillip <96359059+phillipashford@users.noreply.github.com>
2024-05-25 09:44:14 -0400

    Set default variable in App component
    Changed the default selected variable and removed the unnecessary geography change handler.

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,14 @@ import { appConfig } from './config/config';
 function App() {
 
   const [selectedGeography, setSelectedGeography] = useState(appConfig.initialGeography);
-  const [selectedVariable, setSelectedVariable] = useState('No current selection');
+  const [selectedVariable, setSelectedVariable] = useState('Total Population');
   // const [selectedTimePeriod, setSelectedTimePeriod] = useState(appConfig.initialTimePeriod);
-  const handleGeographyChange = (newGeography) => {
-    setSelectedGeography(newGeography);
-    setSelectedVariable('No current selection');
-  };
 
   return (
     <div className="App grid grid-cols-4 h-screen w-screen m-0 p-0">
       <SelectionPanel 
         selectedGeography={selectedGeography}
-        setSelectedGeography={handleGeographyChange}
+        setSelectedGeography={setSelectedGeography}
         selectedVariable={selectedVariable}
         setSelectedVariable={setSelectedVariable}
         // selectedTimePeriod={selectedTimePeriod}
