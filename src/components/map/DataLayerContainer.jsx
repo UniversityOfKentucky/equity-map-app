5805a45bc06d64b1800865940cd4f70931cd742a
Phillip <96359059+phillipashford@users.noreply.github.com>
2024-05-27 08:57:03 -0400

    Integrate legend component into DataLayerContainer
    Added the Legend component to DataLayerContainer.jsx to display a legend on the map. Enhanced data processing and state management to include breaks and colors for accurate data visualization.

--- src/components/map/DataLayerContainer.jsx
+++ src/components/map/DataLayerContainer.jsx
@@ -6,6 +6,7 @@ import constructFetchURL from "../../utils/constructFetchURL";
 import { processData } from "../../utils/dataProcessingUtils";
 import GeoJSONFeatureLayer from "./GeoJSONFeatureLayer";
 import generateVariablesReference from "../../utils/generateVariables";
+import Legend from "./Legend";
 
 const fetchGeoJSON = async (selectedGeography) => {
   const response = await fetch(
@@ -22,19 +23,14 @@ referenceData.variables = generateVariablesReference(referenceData.categories);
 const fetchVariableData = async (selectedGeography, selectedVariable) => {
   const selectedVariablesProps = referenceData.variables[selectedVariable];
 
-  // Construct the main data query URL
   const dataQueryURL = constructFetchURL(selectedGeography, selectedVariable, 'data0');
   const queries = [dataQueryURL];
-  console.log('QUERY', queries);
 
-  // If there's a base filter, add the base data query URL
   if (selectedVariablesProps.baseFilter) {
     const baseQueryURL = constructFetchURL(selectedGeography, selectedVariable, 'base');
     queries.push(baseQueryURL);
-    console.log('QUERIES', queries);
   }
 
-  // Fetch data from all queries
   const responses = await Promise.all(
     queries.map(query => fetch(query).then(response => {
       if (!response.ok) {
@@ -44,24 +40,24 @@ const fetchVariableData = async (selectedGeography, selectedVariable) => {
     }))
   );
 
-  // Aggregate data if there are multiple responses
   const data = responses.length === 1 ? responses[0] : aggregateData(responses[0], responses[1]);
   return data;
 };
 
-// Ensure the aggregateData function returns data in a format similar to ACS responses
 const aggregateData = (data1, data2) => {
   const aggregatedData = data1.map((row, index) => {
     const geoCode = row[row.length - 1];
     const data = row.slice(0, 1).concat(data2[index].slice(0, 1));
     return data.concat(geoCode);
-
   });
 
   return aggregatedData;
 };
 
 const DataLayerContainer = ({ selectedGeography, selectedVariable }) => {
+  const [breaks, setBreaks] = useState([]);
+  const [colors, setColors] = useState([]);
+
   const { data: geoJSONData, error: geoJSONError } = useQuery(
     ["geoJSON", selectedGeography],
     () => fetchGeoJSON(selectedGeography),
@@ -74,44 +70,58 @@ const DataLayerContainer = ({ selectedGeography, selectedVariable }) => {
     { enabled: !!selectedGeography && !!selectedVariable }
   );
 
-  const joinedData = useMemo(() => {
+  useMemo(() => {
     if (geoJSONData && variableData) {
-      const processedData = processData(
-        variableData,
-        selectedVariable,
-        selectedGeography
-      );
+      const { processedData, breaks, colors } = processData(variableData, selectedVariable, selectedGeography);
+      setBreaks(breaks);
+      setColors(colors);
+      // console.log('processedData', processedData);
       const updatedGeoJSONData = { ...geoJSONData };
       updatedGeoJSONData.features.forEach((feature) => {
         const geoCode =
           feature.properties[
             appConfig.geographies[selectedGeography].geoCodeField
           ];
+          // the following line is a ternary operator that checks if the processedData object has a key that matches the geoCode. If it does, it assigns the formattedData and color properties to the feature.properties object. If it doesn't, it assigns the formattedData property the string "No data" and the color property the string "lightgrey".
         const data =
           processedData[geoCode] && processedData[geoCode].formattedData !== 0
             ? processedData[geoCode]
             : { formattedData: "No data", color: "lightgrey" };
         feature.properties = { ...feature.properties, ...data };
       });
-      return updatedGeoJSONData;
+      // setFormat(referenceData.variables[selectedVariable].format);
     }
-    return null;
-  }, [geoJSONData, variableData, selectedGeography, selectedVariable]);
+  }, [geoJSONData, variableData, selectedGeography, selectedVariable]); // runs only when these dependencies change. If you want it to run on initial render and when these 
 
   if (geoJSONError || variableError) {
     return <div>Error loading data</div>;
   }
 
-  if (!geoJSONData || !variableData || !joinedData) {
+  if (!geoJSONData || !variableData) {
     return <div>Loading...</div>;
   }
 
-  return <GeoJSONFeatureLayer data={joinedData} selectedVariable={selectedVariable} />;
+  return (
+    <>
+      <GeoJSONFeatureLayer
+        data={geoJSONData}
+        selectedVariable={selectedVariable}
+      />
+      <Legend
+        selectedVariable={selectedVariable}
+        selectedGeography={selectedGeography}
+        position="leaflet-top leaflet-right"
+        breaks={breaks}
+        colors={colors}
+        // format={format}
+      />
+    </>
+  );
 };
 
 DataLayerContainer.propTypes = {
   selectedGeography: PropTypes.string.isRequired,
-  selectedVariable: PropTypes.string.isRequired,
+  selectedVariable: PropTypes.string.isRequired
 };
 
 export default DataLayerContainer;
