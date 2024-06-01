cc6d6c3edc88c16253dfe31a3390217f9360d02b
Phillip <96359059+phillipashford@users.noreply.github.com>
2024-06-01 14:36:46 -0400

    Add map bounds calculation in MapView
    Enhanced MapView.jsx by wrapping map bounds calculation in a useEffect for better performance. Added placeholder comment for future label-only tile layer. These changes improve map handling and code readability.

--- src/components/map/MapView.jsx
+++ src/components/map/MapView.jsx
@@ -4,16 +4,21 @@ import PropTypes from "prop-types";
 import { appConfig } from "../../config/config";
 import DataLayerContainer from "./DataLayerContainer";
 import FeaturesPanel from "./FeaturesPanel";
+import { useEffect } from "react";
 
 const MapView = ({ selectedGeography, selectedVariable }) => {
   const mapSettings = appConfig.geographies[selectedGeography].mapSettings;
   const center = mapSettings.center;
 
+  useEffect(() => {
   const maxBounds = [
     [center[0] * 1.1, center[1] * 1.1],
     [center[0] * 0.9, center[1] * 0.9],
   ];
   mapSettings.maxBounds = maxBounds;
+  
+  }, [center, mapSettings]);
+
 
   return (
     <div className="col-span-3 relative h-screen">
@@ -23,11 +28,17 @@ const MapView = ({ selectedGeography, selectedVariable }) => {
         {...mapSettings}
         style={{ height: "100%", width: "100%" }}
       >
+        {/* Basemap tile layer */}
         <TileLayer
           url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attributions">CARTO</a>'
           maxZoom="19"
         />
+        {/* Label tile layer */}
+        {/* <TileLayer
+          url="https://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}{r}.png"
+          maxZoom="19"
+        /> */}
         <DataLayerContainer
           selectedGeography={selectedGeography}
           selectedVariable={selectedVariable}
