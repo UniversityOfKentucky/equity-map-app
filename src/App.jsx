f47a3b00cc5d123fe3fed2e7c4433e7cede872eb
Phillip <96359059+phillipashford@users.noreply.github.com>
2024-06-16 20:03:20 -0400

    Integrate tour into App component
    Integrated a tour feature using the reactour library. Added steps to guide users through the application interface, and set the initial state to open the tour on load.

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,64 @@ import MapView from './components/map/MapView';
 import SelectionPanel from './components/sidebar/SelectionPanel';
 import { useState } from 'react';
 import { appConfig } from './config/config';
+import Tour from 'reactour';
 
 function App() {
-
   const [selectedGeography, setSelectedGeography] = useState(appConfig.initialGeography);
   const [selectedVariable, setSelectedVariable] = useState('Total Population');
-  // const [selectedTimePeriod, setSelectedTimePeriod] = useState(appConfig.initialTimePeriod);
+  const [isTourOpen, setIsTourOpen] = useState(true);
+
+  const steps = [
+    {
+      selector: '.geography-selector',
+      content: 'Select your area of study.',
+    },
+    {
+      selector: '.variable-selector',
+      content: 'Choose a category and topic to explore.',
+    },
+    {
+      selector: '.map-container',
+      // Explains the basics of using a leaflet map
+      content: 'Once you have selected your area and topic, the map will display the data. You can zoom in and out, and pan around by clicking and dragging.',
+    },
+    {
+      selector: '.legend-tour-step',
+      // Explains a legend for a layperson
+      content: 'This is the legend. It shows the range of values for your selected topic.',
+    },
+    
+  ];
 
   return (
     <div className="App grid grid-cols-4 h-screen w-screen m-0 p-0">
-      <SelectionPanel 
+      <SelectionPanel
+        className="selection-panel"
         selectedGeography={selectedGeography}
         setSelectedGeography={setSelectedGeography}
         selectedVariable={selectedVariable}
         setSelectedVariable={setSelectedVariable}
-        // selectedTimePeriod={selectedTimePeriod}
-        // setSelectedTimePeriod={setSelectedTimePeriod}
+        setIsTourOpen={setIsTourOpen}
       />
-      <MapView 
+      <MapView
+        className=""
         selectedGeography={selectedGeography}
         selectedVariable={selectedVariable}
-        // selectedTimePeriod={selectedTimePeriod}
-       />
+      />
+      <Tour
+        steps={steps}
+        isOpen={isTourOpen}
+        onRequestClose={() => setIsTourOpen(false)}
+        styles={{
+          options: {
+            // text color the same as tailwind's 'text-gray-900'
+            textColor: '#1a202c',
+          },
+        }}
+
+      />
     </div>
   );
 }
 
-export default App
+export default App;
