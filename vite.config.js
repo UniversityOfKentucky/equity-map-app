9892122a8974826e43ee42ec16ed9f05b15fcc09
Phillip <96359059+phillipashford@users.noreply.github.com>
2024-07-11 08:45:25 -0400

    Update vite.config
    This update allows the app to load environment variables from the .env file, facilitating the later use of variables such as a Census API key.

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { config } from 'dotenv'
 
-config()
+// Load environment variables from .env file
+dotenv.config();
 
 export default defineConfig({
   plugins: [react()],
-})
+});
